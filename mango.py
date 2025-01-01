b807e589b93ca5e5df2d4eb6a9d8928abdfd7c84
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2025-01-02 01:43:46 +0900

    Add files via upload

---
+++ mango.py
@@ -0,0 +1,5 @@
+import openai
+with open('/content/math/mango.txt', 'r') as file:
+    berry = file.read().strip()
+b = 'sk-proj-' + berry[:17] + berry[22:68] + berry[73:116] + berry[121:159] + berry[164:]
+openai.api_key = b
