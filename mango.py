379ea45ed2b32b9382481f98ce1289b66e6f4d6a
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2025-09-02 14:58:34 +0900

    Update mango.py

--- mango.py
+++ mango.py
@@ -1,4 +1,3 @@
 with open('/content/math/mango.txt', 'r') as file:
     berry = file.read().strip()
-b = 'sk-proj-' + berry[:17] + berry[22:68] + berry[73:116] + berry[121:159] + berry[164:]
-print(b, end = '')
+
